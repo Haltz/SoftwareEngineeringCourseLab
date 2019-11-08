5e9a30224f06569bc5a115bcc9e23c33e468cf3b
lxt <lxt@hust.com>
2019-11-08 21:46:51 +0800

    1年级题目函数修正及返回值更改

--- function.py
+++ function.py
@@ -3,26 +3,32 @@ import datetime
 mitime=datetime.datetime.now().microsecond
 random.seed(mitime)  #以当前时间的毫秒为种子
 
-#5以内加减法不含0 返回值:{'fir':第一个运算数,'sec':第二个运算数,'op':运算符,'res':运算结果,'pos':挖空的位置}
+# 5以内加减法不含0 返回值:{'sk':题目,'ak':答案}
 def add_sub_5():
     first = randint(1, 5)
     second = randint(1, 5)
     while (first == second):
         second = randint(1, 5)
     op = randint(0, 2)  # 判断是加法还是减法,0是加法，1是减法
-    pos = randint(0,1)   #判断挖哪个空，0是第一个空，1是第二个空
-    if first > second:
-        if op == 0:
-            return{'fir':second,'sec':(first-second),'op':op,'res':first,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符,res表示运算结果,pos是挖的空的位置
+    pos = randint(0,2)   #判断挖哪个空，0是第一个空，1是第二个空
+    maxs=max(first,second)
+    mins=min(first,second)
+    if op == 0:
+        if pos==0:
+            return{'sk':"__ + "+str(maxs-mins)+" = "+str(maxs),'ak':str(mins)}
+        elif pos==1:
+            return {'sk': str(mins)+" + __ = " + str(maxs), 'ak': str(maxs-mins)}
         else:
-            return{'fir':first,'sec':second,'op':op,'res':(first-second),'pos':pos}
+            return{'sk':str(mins)+" + "+str(maxs-mins)+" = __",'ak':str(max)}
     else:
-        if op == 0:
-            return{'fir':first,'sec':(second-first),'op':op,'res':second,'pos':pos}
+        if pos==0:
+            return{'sk':"__ - "+str(maxs-mins)+" = "+str(mins),'ak':str(maxs)}
+        elif pos==1:
+            return{'sk': str(maxs)+" - __ = " + str(mins), 'ak': str(maxs-mins)}
         else:
-            return{'fir':second,'sec':first,'op':op,'res':(second-first),'pos':pos}
+            return {'sk': str(maxs) + " - " + str(maxs - mins) + " = __", 'ak': str(mins)}
 
-#10以内加减法含0 返回值:{'fir':第一个运算数,'sec':第二个运算数,'op':运算符,'res':运算结果,'pos':挖空的位置}
+# 10以内加减法含0
 def add_sub_10():
     first = randint(0, 9)
     second = randint(0, 9)
@@ -30,30 +36,24 @@ def add_sub_10():
         second = randint(0, 9)
     op = randint(0, 1)  # 判断是加法还是减法,0是加法，1是减法
     pos = randint(0,2)   #判断挖哪个空，0是第一个空，1是第二个空
-    if first > second:
-        if op == 0:
-            return{'fir':second,'sec':(first-second),'op':op,'res':first,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符,res表示运算结果,pos是挖的空的位置
+    maxs = max(first, second)
+    mins = min(first, second)
+    if op == 0:
+        if pos == 0:
+            return {'sk': "__ + " + str(maxs - mins) + " = " + str(maxs), 'ak': str(mins)}
+        elif pos == 1:
+            return {'sk': str(mins) + " + __ = " + str(maxs), 'ak': str(maxs - mins)}
         else:
-            return{'fir':first,'sec':second,'op':op,'res':(first-second),'pos':pos}
+            return {'sk': str(mins) + " + " + str(maxs - mins) + " = __", 'ak': str(max)}
     else:
-        if op == 0:
-            return{'fir':first,'sec':(second-first),'op':op,'res':second,'pos':pos}
+        if pos == 0:
+            return {'sk': "__ - " + str(maxs - mins) + " = " + str(mins), 'ak': str(maxs)}
+        elif pos == 1:
+            return {'sk': str(maxs) + " - __ = " + str(mins), 'ak': str(maxs - mins)}
         else:
-            return{'fir':second,'sec':first,'op':op,'res':(second-first),'pos':pos}
+            return {'sk': str(maxs) + " - " + str(maxs - mins) + " = __", 'ak': str(mins)}
 
-#20以内加法 返回值:{'fir':第一个运算数,'sec':第二个运算数,'res':运算结果}
-def add_20() :
-    first=randint(0,19)
-    second=randint(0,19)
-    pos = randint(0, 2)  # 判断挖哪个空，0是第一个空，1是第二个空
-    while (first==second):
-        second=randint(0,19)
-    if first > second:
-        return{'fir':second,'sec':(first-second),'res':first,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，res表示运算结果
-    else:
-        return{'fir':first,'sec':(second-first),'res':second,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数
-
-#20以内加减法含0 返回值:{'fir':第一个运算数,'sec':第二个运算数,'op':运算符,'res':运算结果}
+# 20以内加减法含0
 def add_sub_20() :
     first=randint(0,19)
     second=randint(0,19)
@@ -61,18 +61,24 @@ def add_sub_20() :
     while (first==second):
         second=randint(0,19)
     op=randint(0,1)  #判断是加法还是减法,0是加法，1是减法
-    if first > second:
-        if op == 0:
-            return{'fir':second,'sec':(first-second),'op':op,'res':first,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符,res表示运算结果
+    maxs = max(first, second)
+    mins = min(first, second)
+    if op == 0:
+        if pos == 0:
+            return {'sk': "__ + " + str(maxs - mins) + " = " + str(maxs), 'ak': str(mins)}
+        elif pos == 1:
+            return {'sk': str(mins) + " + __ = " + str(maxs), 'ak': str(maxs - mins)}
         else:
-            return{'fir':first,'sec':second,'op':op,'res':(first-second),'pos':pos}
+            return {'sk': str(mins) + " + " + str(maxs - mins) + " = __", 'ak': str(max)}
     else:
-        if op == 0:
-            return{'fir':first,'sec':(second-first),'op':op,'res':second,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符
+        if pos == 0:
+            return {'sk': "__ - " + str(maxs - mins) + " = " + str(mins), 'ak': str(maxs)}
+        elif pos == 1:
+            return {'sk': str(maxs) + " - __ = " + str(mins), 'ak': str(maxs - mins)}
         else:
-            return{'fir':second,'sec':first,'op':op,'res':(second-first),'pos':pos}
+            return {'sk': str(maxs) + " - " + str(maxs - mins) + " = __", 'ak': str(mins)}
 
-#100以内加减法2位数（直接求结果）返回值:{'fir':第一个运算数,'sec':第二个运算数,'op':运算符,'res':运算结果}
+# 100以内加减法2位数（直接求结果）
 def add_sub_100() :
     first=randint(0,99)
     second=randint(0,99)
@@ -80,18 +86,24 @@ def add_sub_100() :
         second=randint(0,99)
     op=randint(0,1)  #判断是加法还是减法,0是加法，1是减法
     pos = randint(0, 2)  # 判断挖哪个空，0是第一个空，1是第二个空
-    if first > second:
-        if op == 0:
-            return{'fir':second,'sec':(first-second),'op':op,'res':first,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符,res表示运算结果
+    maxs = max(first, second)
+    mins = min(first, second)
+    if op == 0:
+        if pos == 0:
+            return {'sk': "__ + " + str(maxs - mins) + " = " + str(maxs), 'ak': str(mins)}
+        elif pos == 1:
+            return {'sk': str(mins) + " + __ = " + str(maxs), 'ak': str(maxs - mins)}
         else:
-            return{'fir':first,'sec':second,'op':op,'res':(first-second),'pos':pos}
+            return {'sk': str(mins) + " + " + str(maxs - mins) + " = __", 'ak': str(max)}
     else:
-        if op == 0:
-            return{'fir':first,'sec':(second-first),'op':op,'res':second,'pos':pos}   #fir表示第一个运算数，sec表示第二个运算数，op表示运算符
+        if pos == 0:
+            return {'sk': "__ - " + str(maxs - mins) + " = " + str(mins), 'ak': str(maxs)}
+        elif pos == 1:
+            return {'sk': str(maxs) + " - __ = " + str(mins), 'ak': str(maxs - mins)}
         else:
-            return{'fir':second,'sec':first,'op':op,'res':(second-first),'pos':pos}
+            return {'sk': str(maxs) + " - " + str(maxs - mins) + " = __", 'ak': str(mins)}
 
-#100以内加减法3位数 返回值:{'fir':第一个运算数,'sec':第二个运算数,'op':运算符,'res':运算结果}
+# 100以内加减法3位数
 def add_sub_100_3() :
     first=randint(0,100)
     second=randint(0,100)
@@ -103,63 +115,80 @@ def add_sub_100_3() :
     pos = randint(0, 3)  # 判断挖哪个空，0是第一个空，1是第二个空
     if op1==0:
         if op2==0:
-            if first>second:
-                while(first==third):
-                    third = randint(0, 100)
-                if first>third:
-                    return {'fir': second, 'sec': (first - second),'trd': (first - third), 'op1': op1, 'op2': op2,'res': first,'pos':pos}
-                else:
-                    return {'fir': second, 'sec': (first - second), 'trd': (third-first), 'op1': op1, 'op2': op2,
-                            'res':third,'pos':pos}
+            third = randint(0, 100)
+            maxs = max(first,second,third)
+            mins = min(first,second,third)
+            mids = sum([first,second,third])-maxs-mins
+            if pos==0:
+                return {'sk': '__ + ' + str(mids - mins) + ' + ' + str(maxs - mids) + ' = ' + str(maxs),
+                        'ak': str(mins)}
+            elif pos==1:
+                return {'sk': str(mins) +' + __ + ' + str(maxs - mids) + ' = ' + str(maxs),
+                        'ak': str(mids-mins)}
+            elif pos==2:
+                return {'sk': str(mins) +' + ' + str(mids - mins) + ' + __ = ' + str(maxs),
+                        'ak': str(maxs-mids)}
             else:
-                while (second == third):
-                    third = randint(0, 100)
-                if second > third:
-                    return {'fir': first, 'sec': (second-first), 'trd': (second - third), 'op1': op1, 'op2': op2,
-                            'res': second,'pos':pos}
-                else:
-                    return {'fir': first, 'sec': (second-first), 'trd': (third - second), 'op1': op1, 'op2': op2,
-                            'res': third,'pos':pos}
+                return {'sk': str(mins) + ' + ' + str(mids - mins) + ' + '+ str(mids - mins) + ' = __' ,
+                        'ak': str(maxs)}
         else:
-            if first>second:
-                while(first<=third):
-                    third = randint(0, 100)
-                    return {'fir': second, 'sec': (first - second),'trd': third, 'op1': op1, 'op2': op2,'res': (first-third),'pos':pos}
+            while(first<third):
+                third = randint(0, 100)
+                maxs=max(first,second)
+                mins=min(first,second)
+            if pos==0:
+                return{'sk':'__ + '+str(maxs-mins)+' - '+str(third)+' = '+str(maxs-third),'ak':str(mins)}
+            elif pos==1:
+                return {'sk': str(mins)+' + __ - ' + str(third) + ' = ' + str(maxs - third),
+                        'ak': str(maxs-mins)}
+            elif pos==2:
+                return {'sk': str(mins)+' + ' + str(maxs - mins) + ' - __ = '  + str(maxs - third),
+                        'ak': str(third)}
             else:
-                while (second <= third):
-                    third = randint(0, 100)
-                    return {'fir': first, 'sec': (second-first), 'trd': third, 'op1': op1, 'op2': op2,
-                            'res': (second-third),'pos':pos}
+                return {'sk': str(mins) + ' + ' + str(maxs-mins) + ' - '+str(third)+' = __',
+                        'ak': str(maxs-third)}
     else:
         if op2==0:
-            if first>second:
-                while((second+third)>=100):
-                    third = randint(0, 100)
-                    return {'fir': first, 'sec': (first - second),'trd': third, 'op1': op1, 'op2': op2,'res': second+third,'pos':pos}
+            maxs=max(first,second)
+            mins=min(first,second)
+            while((mins+third)>=100):
+                third = randint(0, 100)
+            if pos==0:
+                return{'sk':'__ - '+str(maxs-mins)+' + '+str(third)+' = '+str(mins+third),'ak':str(maxs)}
+            elif pos==1:
+                return {'sk': str(maxs)+' - __ + ' + str(third) + ' = ' + str(mins+ third),
+                        'ak': str(maxs-mins)}
+            elif pos==2:
+                return {'sk': str(maxs)+' - ' + str(maxs - mins) + ' + __ = '  + str(mins+ third),
+                        'ak': str(third)}
             else:
-                while ((first+third)>=100):
-                    third = randint(0, 100)
-                    return {'fir': second, 'sec': (second-first), 'trd':third , 'op1': op1, 'op2': op2,
-                            'res': (third+first),'pos':pos}
+                return {'sk': str(maxs) + ' - ' + str(maxs-mins) + ' + '+str(third)+' = __',
+                        'ak': str(mins+third)}
         else:
-            if first>second:
-                while(second<=third):
-                    third = randint(0, 100)
-                    return {'fir': first, 'sec': second,'trd': third, 'op1': op1, 'op2': op2,'res': (first-third-second),'pos':pos}
+            third = randint(0, 100)
+            maxs=max(first,second,third)
+            mins=min(first,second,third)
+            mids=sum([first,second,third])-maxs-mins
+            if pos==0:
+                return{'sk':'__ - '+str(maxs-mids)+' - '+str(mids-mins)+' = '+str(mins),'ak':str(maxs)}
+            elif pos==1:
+                return {'sk': str(maxs)+' - __ - ' + str(mids-mins) + ' = ' + str(mins),
+                        'ak': str(maxs-mids)}
+            elif pos==2:
+                return {'sk': str(maxs)+' - ' + str(maxs - mids) + ' - __ = '  + str(mins),
+                        'ak': str(mids-mins)}
             else:
-                while (first <= third):
-                    third = randint(0, 100)
-                    return {'fir': second, 'sec': first, 'trd': third, 'op1': op1, 'op2': op2,
-                            'res': (second-third-first),'pos':pos}
+                return {'sk': str(maxs) + ' - ' + str(maxs-mids) + ' - '+str(mids-mins)+' = __',
+                        'ak': str(mins)}
 
-#口诀表内乘法
+# 口诀表内乘法
 def mul_biao():
     first = randint(1, 9)
     second = randint(1, 9)
     pos=randint(0,2)
     return {'fir':second,'sec':first,'op':2,'res':(first*second),'pos':pos}
 
-#口诀表内除法
+# 口诀表内除法
 def div_biao():
     first = randint(1, 9)
     second = randint(1, 9)
