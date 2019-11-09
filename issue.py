4a2609c7b4b590c3cfa764304f2210c461f74641
lxt <lxt@hust.com>
2019-11-09 10:52:14 +0800

    总生成题目函数（部分完成）

---
+++ issue.py
@@ -0,0 +1,70 @@
+from san import*
+from function import*
+from random import *
+
+def issue(grade,mode):
+    r = randint(0, 10)
+    #1年级
+    if grade==1:
+        #简单模式
+        if mode=='easy':
+            if r<=7:
+                return add_sub_5 ()
+            elif r<=9:
+                return add_sub_10()
+            else :
+                return add_sub_20()
+        #普通模式
+        elif mode=='normal':
+            if r<=1:
+                return add_sub_5 ()
+            elif r<=5:
+                return add_sub_10()
+            elif r<=8:
+                return add_sub_20()
+            else:
+                return add_sub_100()
+        #困难模式
+        else:
+            if r<=1:
+                return add_sub_10()
+            elif r<=4:
+                return add_sub_20()
+            elif r<=8:
+                return add_sub_100()
+            else:
+                return add_sub_100_3()
+    #2年级
+    elif grade==2:
+        #简单模式
+        if mode=='easy':
+            if r<=3:
+                return add_sub_100()
+            elif r<=7:
+                return add_sub_100_3()
+            else:
+                return mul_biao()
+        #普通模式
+        elif mode=='normal':
+            if r<=1:
+                return add_sub_100_3()
+            elif r<=4:
+                return mul_biao()
+            elif r<=7:
+                return div_biao()
+            else:
+                return div_remainder()
+        #困难模式
+        else:
+            if r<=2:
+                return div_biao()
+            elif r<=5:
+                return div_remainder()
+            elif r<=9:
+                return mix_100_3()
+            else :
+                return add_sub_1w ()
+    #3年级
+
+
+
